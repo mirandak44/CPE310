f764ace620d15ef19765e957d93223ecbc8a5e73
AlexisDep0 <and00016@mix.wvu.edu>
2024-10-29 12:05:09 -0400

    Alexis' Code for Assembler Project

--- MFLO.c
+++ MFLO.c
@@ -1,11 +1,110 @@
+/*
+Name:Alexis DePue
+Group Members:Aishat Banire, Emily Kissner, Miranda Kirtley
+Code Modified by Alexis: OR.c, ORI.c, MULT.c, MFLO.c
+*/
 #include "Instruction.h"
 
 void mflo_reg_assm(void) {
-	state = WRONG_COMMAND;
+	// Checking that the op code matches
+		// strcmp(string1, string2) return 0 if they match
+	if (strcmp(OP_CODE, "MFLO") != 0) {
+		// If the op code doesnt match, this isnt the correct command
+		state = WRONG_COMMAND;
+		return;
+	}
+
+	/*
+		Checking the type of parameters
+	*/
+
+	// Generally the first parameter should always be a register
+	if (PARAM1.type != REGISTER) {
+		state = MISSING_REG;
+		return;
+	}
+
+	if (PARAM2.type != REGISTER) {
+		state = MISSING_REG;
+		return;
+	}
+
+	if (PARAM3.type != REGISTER) {
+		state = MISSING_REG;
+		return;
+	}
+
+
+
+	/*
+		Checking the value of parameters
+	*/
+
+	// Rd should be 31 or less
+	if (PARAM1.value > 31) {
+		state = INVALID_REG;
+		return;
+	}
+
+	// Rs should be 31 or less
+	if (PARAM2.value > 31) {
+		state = INVALID_REG;
+		return;
+	}
+
+	if (PARAM3.value > 31) {
+		state = INVALID_REG;
+		return;
+	}
+
+
+	/*
+		Putting the binary together
+	*/
+
+	// Set the opcode
+	setBits_num(31, 0, 6);
+
+	// Set the funct 
+	setBits_str(5, "010010");
+
+	// set Rd
+	setBits_num(15, PARAM1.value, 5);
+
+	// set Rs
+	setBits_num(25, PARAM2.value, 5);
+
+	// set Rt
+	setBits_num(20, PARAM3.value, 5);
+
+	// tell the system the encoding is done
+	state = COMPLETE_ENCODE;
 }
 
 void mflo_reg_bin(void) {
-	state = WRONG_COMMAND;
+	// Check if the op code bits match
+	if (checkBits(31, "000000") != 0 || checkBits(5, "010010") != 0) {
+		state = WRONG_COMMAND;
+		return;
+	}
+
+	// If the op code bits match, then the rest can be read as correctly
+
+	/* Finding values in the binary */
+	uint32_t Rd = getBits(15, 5);
+	uint32_t Rs = getBits(25, 5);
+	uint32_t Rt = getBits(20, 5);
+
+	/* Setting Instruction values */
+	setOp("MFLO");
+	//setCond_num(cond);
+	setParam(1, REGISTER, Rd);  // destination register
+	setParam(2, REGISTER, Rs);  // First source register operand
+	setParam(3, REGISTER, Rt);  // Second source register operand
+
+
+	// Tell the system the decoding is done
+	state = COMPLETE_DECODE;
 }
 
 
--- OR.c
+++ OR.c
@@ -1,11 +1,124 @@
+/*
+Name:Alexis DePue
+Group Members:Aishat Banire, Emily Kissner, Miranda Kirtley
+Code Modified by Alexis: OR.c, ORI.c, MULT.c, MFLO.c
+*/
 #include "Instruction.h"
 
 void or_reg_assm(void) {
-	state = WRONG_COMMAND;
+	// Checking that the op code matches
+		// strcmp(string1, string2) return 0 if they match
+	if (strcmp(OP_CODE, "OR") != 0) {
+		// If the op code doesnt match, this isnt the correct command
+		state = WRONG_COMMAND;
+		return;
+	}
+
+	/*
+		Checking the type of parameters
+	*/
+
+	// Generally the first parameter should always be a register rd
+	if (PARAM1.type != REGISTER) {
+		state = MISSING_REG;
+		return;
+	}
+
+	// This is AND register, so param 2 needs to be a register rs
+	if (PARAM2.type != REGISTER) {
+		state = MISSING_REG;
+		return;
+	}
+
+	// This is AND register, so param 3 needs to be a register rt
+	if (PARAM3.type != REGISTER) {
+		state = MISSING_REG;
+		return;
+	}
+
+
+	/*
+		Checking the value of parameters
+	*/
+
+	// Rd should be 31 or less
+	if (PARAM1.value > 31) {
+		state = INVALID_REG;
+		return;
+	}
+
+	// Rs should be 31 or less
+	if (PARAM2.value > 31) {
+		state = INVALID_REG;
+		return;
+	}
+
+	// Rt should be 31 or less
+	if (PARAM3.value > 31) {
+		state = INVALID_REG;
+		return;
+	}
+
+
+
+	/*
+		Putting the binary together
+	*/
+
+	// Set the opcode
+	setBits_num(31, 0, 6);
+
+	// Set the funct 
+	setBits_str(5, "100101");
+
+	// set Rd
+	setBits_num(15, PARAM1.value, 5);
+
+	// set Rs
+	setBits_num(25, PARAM2.value, 5);
+
+	// set Rt
+	setBits_num(20, PARAM3.value, 5);
+
+	// tell the system the encoding is done
+	state = COMPLETE_ENCODE;
 }
 
 void or_reg_bin(void) {
-	state = WRONG_COMMAND;
+	// Check if the op code bits match
+	// check_bits(start_bit, bit_string) returns 0 if the bit_string matches
+	//  any x will be skipped
+	// If the manual shows (0), then the value of that bit doesnt matter
+	if (checkBits(31, "000000") != 0 || checkBits(5, "100101") != 0) {
+		state = WRONG_COMMAND;
+		return;
+	}
+
+	// If the op code bits match, then the rest can be read as correctly
+
+	/*
+		Finding values in the binary
+	*/
+	// getBits(start_bit, width)
+	uint32_t Rd = getBits(15, 5);
+	uint32_t Rs = getBits(25, 5);
+	uint32_t Rt = getBits(20, 5);
+
+
+	/*
+		Setting Instuciton values
+	*/
+
+	setOp("OR");
+	//setCond_num(cond);
+	//setParam(param_num, param_type, param_value)
+	setParam(1, REGISTER, Rd); //destination
+	setParam(2, REGISTER, Rs); //first source register operand
+	setParam(3, REGISTER, Rt); //second source register operand
+
+
+	// tell the system the decoding is done
+	state = COMPLETE_DECODE;
 }
 
 
