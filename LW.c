82aee5a0d080e96e9927bff55f9d7d266d499af3
mirandak44 <mbk00007@mix.wvu.edu>
2024-10-31 13:58:41 -0400

    Miranda Kirtley's code (DIV, LW, LUI, MFHI)

--- LW.c
+++
@@ -1,9 +0,0 @@
-#include "Instruction.h"
-
-void lw_immd_assm(void) {
-	
-}
-
-void lw_immd_bin(void) {
-	
-}
--- SW.c
+++
@@ -1,9 +0,0 @@
-#include "Instruction.h"
-
-void sw_immd_assm(void) {
-	state = WRONG_COMMAND;
-}
-
-void sw_immd_bin(void) {
-	state = WRONG_COMMAND;
-}
