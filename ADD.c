716312136830809e70e0f74b39006cf78ba6c635
emkissner <kissneremily@gmail.com>
2024-11-01 08:49:09 -0400

    ADD.c

--- ADD.c
+++ ADD.c
@@ -1,11 +1,120 @@
+/*
+Name: Emily Kissner
+Group Members: Aishat Banire, Alexis DePue, Miranda Kirtley
+Code Modified by Emily Kissner: ADD.c ADDI.c BEQ.c BNE.c
+*/
+
+
 #include "Instruction.h"
 
 void add_reg_assm(void) {
-	state = WRONG_COMMAND;
+    // Checking that the op code matches
+    // strcmp(string1, string2) return 0 if they match
+    if (strcmp(OP_CODE, "ADD") != 0) {
+        // If the op code doesn't match, this isn't the correct command
+        state = WRONG_COMMAND;
+        return;
+    }
+
+    /*
+        Checking the type of parameters
+    */
+
+    // Generally the first parameter should always be a register
+    if (PARAM1.type != REGISTER) {
+        state = MISSING_REG;
+        return;
+    }
+
+    // This is ADD register, so param 2 needs to be a register
+    if (PARAM2.type != REGISTER) {
+        state = MISSING_REG;
+        return;
+    }
+
+    // This is ADD register, so param 3 needs to be a register
+    if (PARAM3.type != REGISTER) {
+        state = MISSING_REG;
+        return;
+    }
+
+    /*
+        Checking the value of parameters
+    */
+
+    // Rd should be 31 or less
+    if (PARAM1.value > 31) {
+        state = INVALID_REG;
+        return;
+    }
+
+    // Rs should be 31 or less
+    if (PARAM2.value > 31) {
+        state = INVALID_REG;
+        return;
+    }
+
+    // Rt should be 31 or less
+    if (PARAM3.value > 31) {
+        state = INVALID_REG;
+        return;
+    }
+    
+    /*
+        Putting the binary together
+    */
+
+    // Set the opcode
+    setBits_num(31, 0, 6); // Opcode remains the same for R-type instructions
+
+    // Set the funct
+    setBits_str(5, "100000");
+
+    // set Rd
+    setBits_num(15, PARAM1.value, 5);
+
+    // set Rs
+    setBits_num(25, PARAM2.value, 5);
+
+    // set Rt
+    setBits_num(20, PARAM3.value, 5);
+
+    // tell the system the encoding is done
+    state = COMPLETE_ENCODE;
 }
 
 void add_reg_bin(void) {
-	state = WRONG_COMMAND;
+    // Check if the op code bits match
+    // check_bits(start_bit, bit_string) returns 0 if the bit_string matches
+    //  any x will be skipped
+    // If the manual shows (0), then the value of that bit doesn't matter
+    if (checkBits(31, "000000") != 0 || checkBits(5, "100000") != 0) {
+        state = WRONG_COMMAND;
+        return;
+    }
+
+    // If the op code bits match, then the rest can be read as correctly
+
+    /*
+        Finding values in the binary
+    */
+    // getBits(start_bit, width)
+    uint32_t Rd = getBits(15, 5);
+    uint32_t Rs = getBits(25, 5);
+    uint32_t Rt = getBits(20, 5);
+
+
+    /*
+        Setting Instruction values
+    */
+
+    setOp("ADD");
+    //setCond_num(cond);
+    //setParam(param_num, param_type, param_value)
+    setParam(1, REGISTER, Rd); //destination
+    setParam(2, REGISTER, Rs); //first source register operand
+    setParam(3, REGISTER, Rt); //second source register operand
+
+    // tell the system the decoding is done
+    state = COMPLETE_DECODE;
 }
-
-
